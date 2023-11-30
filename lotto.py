88f81df89eaf564a5ca68f0c4ef349cd61f63a09
Da5idEducator <adamcsik.david@levaygimnazium.hu>
2023-11-30 12:22:54 +0100

    Lotto sorsolas program

---
+++ lotto.py
@@ -0,0 +1,10 @@
+# Lottó számok listájának előállítása
+
+lottoszamok = []
+
+for i in range(1, 91):
+    lottoszamok.append(i)
+    
+print(lottoszamok)
+
+
---
+++ lottosorsolas.py
@@ -0,0 +1,11 @@
+# Lottó számok listájának a létrehozása
+
+lottoGomb = []
+
+for i in range (1, 91):
+    lottoGomb.append(i)
+    
+print(lottoGomb)
+
+
+
