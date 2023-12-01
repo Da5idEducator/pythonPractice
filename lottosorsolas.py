5e55c58300bd9259024d875c79dd94dffe371673
Da5idEducator <adamcsik.david@levaygimnazium.hu>
2023-12-01 10:01:11 +0100

    Lotto algoritmus tisztazat

--- lottosorsolas.py
+++ lottosorsolas.py
@@ -4,12 +4,13 @@ import random
 lottoGomb = []
 kihuzottSzamok = []
 tipp = []
-szam = 0
+# szam = 0
+counter = 0
 
 for i in range(0, 5):
     szam = int(input("Kérek egy 1 és 90 közé eső számot: "))
     tipp.append(szam)
-    
+
 for i in range (1, 91):
     lottoGomb.append(i)
 
